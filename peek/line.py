e2fe8e5f8bfcfea6ae4b8faeb9bae0b565e75607
Simon AM <simon@agius-muscat.net>
2017-02-12 00:19:43 +0100

    Added retrieval tests

--- peek/line.py
+++ peek/line.py
@@ -4,6 +4,40 @@ class Line:
         self._timestamp = row[1]
         self._verb = row[2]
         self._path = row[3]
-        self._byte_count = row[4]
-        self._referrer = row[5]
-        self._user_agent = row[6]
+        self._status = row[4]
+        self._byte_count = row[5]
+        self._referrer = row[6]
+        self._user_agent = row[7]
+
+    @property
+    def ip_address(self):
+        return self._ip_address
+
+    @property
+    def timestamp(self):
+        # TODO: Convert to number
+        return self._timestamp
+
+    @property
+    def verb(self):
+        return self._verb
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def status(self):
+        return int(self._status)
+
+    @property
+    def byte_count(self):
+        return int(self._byte_count)
+
+    @property
+    def referrer(self):
+        return self._referrer
+
+    @property
+    def user_agent(self):
+        return self._user_agent
--- tests/unit/test_line.py
+++ tests/unit/test_line.py
@@ -1,9 +1,28 @@
+import pytest
+
+from peek.line_parser import LineParser
+
+
+test_line = LineParser.parse_line(line='127.0.0.1 - - [01/Jan/1970:00:00:01] "GET / HTTP/1.1" 200 193 "-" "Python"')
+# 127.0.0.1 - - [01/Jan/1970:00:00:01] "GET / HTTP/1.1" 200 193 "-" "Python"
+
+
 class TestLineInstantiation:
-    def test_passing_valid_variables(self):
-        pass
+    @pytest.mark.parametrize('expected,actual', [
+        ('127.0.0.1', test_line.ip_address),
+        (1, test_line.timestamp),
+        ('GET', test_line.verb),
+        ('/', test_line.path),
+        (200, test_line.status),
+        (193, test_line.byte_count),
+        ('-', test_line.referrer),
+        ('Python', test_line.user_agent)
+    ])
+    def test_retrieval(self, expected, actual):
+        assert expected == actual
 
 
 class TestLineExceptions:
     def test_passing_invalid_url_throws_exception(self):
-        pass
+        assert False is True
 
