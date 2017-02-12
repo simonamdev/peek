0a01d108917eacde73c5853edd5d71153bcc3e84
Simon AM <simon@agius-muscat.net>
2017-02-12 22:41:00 +0100

    Added reporting to peek

--- peek/peek_runner.py
+++ peek/peek_runner.py
@@ -1,6 +1,9 @@
 import time
 
+from peek.line import Line
+from peek.line_parser import LineParser
 from peek.log_file import LogFile
+from peek.log_statistics import LogStatistics
 
 
 def watch_file(file_path, delay=0.1):
@@ -15,12 +18,19 @@ def watch_file(file_path, delay=0.1):
 
 
 class PeekRunner:
-    def __init__(self, file_path):
+    def __init__(self, file_path, persist=False):
         self._log_file = LogFile(file_path=file_path)
         self._original_line_count = self._log_file.length
         self._lines_parsed = 0
+        self._log_statistics = LogStatistics(persist=persist)
 
     def parse_logs(self):
-        for incoming_connection in watch_file(file_path=self._log_file.file_path):
+        print('Peeking into nginx logs')
+        for incoming_connection_log in watch_file(file_path=self._log_file.file_path):
             self._lines_parsed += 1
-            print('[{}] - {}'.format(self._lines_parsed, incoming_connection))
+            current_line = Line(line_contents=LineParser.parse_line(line=incoming_connection_log))
+            self._log_statistics.insert_line(line=current_line)
+            self.report_statistics()
+
+    def report_statistics(self):
+        pass
