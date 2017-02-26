0081e7a4396138e08ab09b16bc733deef7e23f68
Simon AM <simon@agius-muscat.net>
2017-02-26 11:27:59 +0100

    Restart the file watcher when the log file is reset

--- peek/peek_runner.py
+++ peek/peek_runner.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from peek.line_parser import LineParser
@@ -23,12 +24,26 @@ class PeekRunner:
         self._log_file = LogFile(file_path=file_path)
         self._original_line_count = self._log_file.length
         self._lines_parsed = 0
+        self._log_file_size = 0
+        self._previous_log_file_size = 0
         self._log_statistics = LogStatistics(persist=True)
 
     def parse_logs(self):
         print('Peeking into nginx logs')
-        for incoming_connection_log in watch_file(file_path=self._log_file.file_path):
-            self._lines_parsed += 1
-            current_line = LineParser.parse_line(line=incoming_connection_log)
-            self._log_statistics.insert_line(line=current_line)
-            print('Lines Parsed: {}'.format(self._lines_parsed), end='\r')
+        while True:
+            # Reset  the log file watcher when the new log size is smaller than the last size
+            log_file_watcher = watch_file(file_path=self._log_file.file_path)
+            self._previous_log_file_size = 0
+            for incoming_connection_log in log_file_watcher:
+                self._lines_parsed += 1
+                log_file_size_mb = os.path.getsize(self._log_file.file_path) / (1024 * 1024)
+                if log_file_size_mb < self._previous_log_file_size:
+                    self._previous_log_file_size = 0
+                    break
+                current_line = LineParser.parse_line(line=incoming_connection_log)
+                self._log_statistics.insert_line(line=current_line)
+                print('Lines Parsed: {} Log Size: {} MB'.format(
+                    self._lines_parsed,
+                    round(log_file_size_mb, 2)), end='\r')
+                self._previous_log_file_size = log_file_size_mb
+
